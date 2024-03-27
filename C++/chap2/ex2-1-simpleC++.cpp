57fffc77163d47c2cc42b1f2f079e4270043b6b6
freshmea <freshmea@naver.com>
2024-03-27 17:45:02 +0900

    C++ 코드에 문자열과 벡터를 추가했습니다.***

--- C++/chap2/ex2-1-simpleC++.cpp
+++ C++/chap2/ex2-1-simpleC++.cpp
@@ -7,6 +7,8 @@
  */
 
 #include <iostream>
+#include <string>
+#include <vector>
 
 /**
  * @brief The main function of the program.
@@ -14,7 +16,21 @@
  * @return 0 
  */
 int main() {
+    char c[] = "hello";
+    std::vector<char> v;
+    v.push_back('h');
+    v.push_back('e');
+    v.push_back('l');
     std::cout << "Hello, World!" << std::endl;
     std::cout << "첫 번째 맛보기 C++ 프로그램입니다." << std::endl;
+    std::string teststring(v.begin(), v.end());
+    for (int i = 0; i < 10; i++){
+        teststring += std::to_string(i);
+        std::cout << teststring << std::endl;
+    }
+    for (auto i : v){
+        teststring += i;
+        std::cout << i << std::endl;
+    }
     return 0;
 }
