705c00f7ee0a811a05518a796a449bc44ccffd63
freshmea <freshmea@naver.com>
2024-04-22 13:46:19 +0900

    aa

--- openCV/ch16/dnnmnist/main.cpp
+++ openCV/ch16/dnnmnist/main.cpp
@@ -7,9 +7,28 @@ using namespace std;
 
 void on_mouse(int event, int x, int y, int flags, void* userdata);
 
-int main()
+int main(int argc, char* argv[])
 {
-	Net net = readNet("../mnist_cnn.pb");
+	if (argc < 2) {
+        cerr << "Usage: " << argv[0] << " <model_path>" << endl;
+        return -1;
+    }
+
+	// argv[1] is  .onnx file
+	std::string filename = argv[1];
+	// get extension before .
+	std::string extension = filename.substr(filename.find_last_of(".") + 1);
+	Net net;
+	printf("extension: %s\n", extension.c_str());
+	if (extension == "onnx") {
+		net = readNetFromONNX(argv[1]);
+	// argv[1] is .pb file
+	} else if (extension == "pb") {
+		net = readNet(argv[1]);
+	} else {
+		cerr << "Model file should be .onnx or .pb" << endl;
+		return -1;
+	}
 
 	if (net.empty()) {
 		cerr << "Network load failed!" << endl;
