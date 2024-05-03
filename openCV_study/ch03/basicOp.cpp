d8d6c2b576fe2601455fe97f0f04bcda2d4a80d8
freshmea <freshmea@naver.com>
2024-05-03 22:51:40 +0900

    indent

--- openCV_study/ch03/basicOp.cpp
+++ openCV_study/ch03/basicOp.cpp
@@ -7,21 +7,21 @@ using namespace cv;
 using namespace std;
 
 int main() {
-  // Point 예제
-  Point pt1;
-  pt1.x = 5;
-  pt1.y = 10;
-  Point pt2(10, 20);
-  cout << pt1 << endl;
-  cout << pt2 << endl;
-  cout << pt1 + pt2 << endl;
+	// Point 예제
+	Point pt1;
+	pt1.x = 5;
+	pt1.y = 10;
+	Point pt2(10, 20);
+	cout << pt1 << endl;
+	cout << pt2 << endl;
+	cout << pt1 + pt2 << endl;
 
-  // Size 예제
-  Size sz1;
-  sz1.width = 10;
-  sz1.height = 20;
-  Size sz2(100, 200);
-  cout << sz1 << sz2 << sz1.area() << sz1.aspectRatio() << endl;
+	// Size 예제
+	Size sz1;
+	sz1.width = 10;
+	sz1.height = 20;
+	Size sz2(100, 200);
+	cout << sz1 << sz2 << sz1.area() << sz1.aspectRatio() << endl;
 
-  return 0;
+	return 0;
 }
--- openCV_study/ch03/matOp2.cpp
+++ openCV_study/ch03/matOp2.cpp
@@ -6,23 +6,23 @@ using namespace std;
 String folder = "/home/aa/kuIotBigdataClass/openCV_study/data/";
 
 int main() {
-  Mat img1 = imread(folder + "dog.bmp");
-  Mat img2 = img1;
-  Mat img3;
-  img3 = img1;
+	Mat img1 = imread(folder + "dog.bmp");
+	Mat img2 = img1;
+	Mat img3;
+	img3 = img1;
 
-  Mat img4 = img1.clone();
-  Mat img5;
-  img1.copyTo(img5);
+	Mat img4 = img1.clone();
+	Mat img5;
+	img1.copyTo(img5);
 
-  img1.setTo(Scalar(0, 255, 255)); // 노란색
+	img1.setTo(Scalar(0, 255, 255)); // 노란색
 
-  imshow("img1", img1);
-  imshow("img2", img2);
-  imshow("img3", img3);
-  imshow("img4", img4);
-  imshow("img5", img5);
-  waitKey(0);
+	imshow("img1", img1);
+	imshow("img2", img2);
+	imshow("img3", img3);
+	imshow("img4", img4);
+	imshow("img5", img5);
+	waitKey(0);
 
-  return 0;
+	return 0;
 }
