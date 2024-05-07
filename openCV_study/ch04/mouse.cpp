3febe273d469487070353e9f4d92ea1e5716b8a2
freshmea <freshmea@naver.com>
2024-05-07 11:32:47 +0900

    mousecallback

---
+++ openCV_study/ch04/mouse.cpp
@@ -0,0 +1,51 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+String folder = "/home/aa/kuIotBigdataClass/openCV_study/data/";
+void onMouse(int event, int x, int y, int flags, void *);
+Point ptOld;
+Point ptNew;
+Mat img;
+Scalar white = Scalar(255, 255, 255);
+Scalar yellow = Scalar(0, 255, 255);
+Scalar blue = Scalar(255, 0, 0);
+Scalar green = Scalar(0, 255, 0);
+Scalar red = Scalar(0, 0, 255);
+bool flag = false;
+
+int main() {
+	img = imread(folder + "lenna.bmp", IMREAD_COLOR);
+
+	namedWindow("img");
+	setMouseCallback("img", onMouse);
+
+	imshow("img", img);
+	waitKey(0);
+	destroyAllWindows();
+	return 0;
+}
+
+void onMouse(int event, int x, int y, int flags, void *) {
+	switch (event) {
+	case EVENT_LBUTTONDOWN:
+		ptOld = Point(x, y);
+		cout << "Event Left Button Down: " << x << ", " << y << endl;
+		flag = true;
+		break;
+	case EVENT_LBUTTONUP:
+		ptNew = Point(x, y);
+		cout << "Event Left Button Up: " << x << ", " << y << endl;
+		flag = false;
+		break;
+	case EVENT_MOUSEMOVE:
+		ptOld = Point(x, y);
+		if (flag & EVENT_FLAG_LBUTTON) {
+			line(img, ptOld, Point(x, y), blue, 5);
+			imshow("img", img);
+			// waitKey(3);
+		}
+		break;
+	}
+}
