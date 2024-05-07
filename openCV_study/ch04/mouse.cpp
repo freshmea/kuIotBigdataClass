2cd7b5b17e037a05307e522e364ed0b0909efcd5
freshmea <freshmea@naver.com>
2024-05-07 11:50:42 +0900

    aa

--- openCV_study/ch04/mouse.cpp
+++ openCV_study/ch04/mouse.cpp
@@ -5,46 +5,49 @@ using namespace cv;
 using namespace std;
 String folder = "/home/aa/kuIotBigdataClass/openCV_study/data/";
 void onMouse(int event, int x, int y, int flags, void *);
-Point ptOld;
-Point ptNew;
-Mat img;
-Scalar white = Scalar(255, 255, 255);
-Scalar yellow = Scalar(0, 255, 255);
-Scalar blue = Scalar(255, 0, 0);
-Scalar green = Scalar(0, 255, 0);
-Scalar red = Scalar(0, 0, 255);
-bool flag = false;
+struct MyData {
+	Point ptOld;
+	Point ptNew;
+	Mat img;
+	Scalar white = Scalar(255, 255, 255);
+	Scalar yellow = Scalar(0, 255, 255);
+	Scalar blue = Scalar(255, 0, 0);
+	Scalar green = Scalar(0, 255, 0);
+	Scalar red = Scalar(0, 0, 255);
+	bool flag = false;
+};
 
 int main() {
-	img = imread(folder + "lenna.bmp", IMREAD_COLOR);
+	MyData myData;
+	myData.img = imread(folder + "lenna.bmp", IMREAD_COLOR);
 
 	namedWindow("img");
-	setMouseCallback("img", onMouse);
+	setMouseCallback("img", onMouse, (void *)&myData);
 
-	imshow("img", img);
+	imshow("img", myData.img);
 	waitKey(0);
 	destroyAllWindows();
 	return 0;
 }
 
-void onMouse(int event, int x, int y, int flags, void *) {
+void onMouse(int event, int x, int y, int flags, void *myData) {
+	MyData *ptr = (MyData *)myData;
 	switch (event) {
 	case EVENT_LBUTTONDOWN:
-		ptOld = Point(x, y);
+		ptr->ptOld = Point(x, y);
 		cout << "Event Left Button Down: " << x << ", " << y << endl;
-		flag = true;
+		ptr->flag = true;
 		break;
 	case EVENT_LBUTTONUP:
 		// ptNew = Point(x, y);
 		cout << "Event Left Button Up: " << x << ", " << y << endl;
-		flag = false;
+		ptr->flag = false;
 		break;
 	case EVENT_MOUSEMOVE:
-		if (flag & EVENT_FLAG_LBUTTON) {
-			line(img, ptOld, Point(x, y), blue, 5);
-			imshow("img", img);
-			ptOld = Point(x, y);
-			// waitKey(3);
+		if (ptr->flag & EVENT_FLAG_LBUTTON) {
+			line(ptr->img, ptr->ptOld, Point(x, y), ptr->blue, 5);
+			imshow("img", ptr->img);
+			ptr->ptOld = Point(x, y);
 		}
 		break;
 	}
