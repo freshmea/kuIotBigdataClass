67d79a411ebc20205d348d936a4b46bac0619425
freshmea <freshmea@naver.com>
2024-05-24 17:03:08 +0900

    tag

--- pythonData/beautifulsoup1.py
+++ pythonData/beautifulsoup1.py
@@ -6,7 +6,7 @@ def main():
     with open(folder + "books.xml", "r", encoding="utf8") as f:
         xml = f.read()
         soup = BeautifulSoup(xml, "lxml")
-    
+
     for book_info in soup.find_all("author"):
         print(book_info)
         print(book_info.get_text())
