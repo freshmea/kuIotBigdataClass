ede50d1860bb269a34a511cd659d4c76c8965d92
freshmea <freshmea@naver.com>
2024-05-24 14:55:02 +0900

    logging config

--- pythonData/configParserTest.py
+++ pythonData/configParserTest.py
@@ -9,7 +9,7 @@ def main():
     parser.add_argument("-a", "--a_value", dest="a", help="A integers", type=int, default= 10)
     parser.add_argument("-b", "--b_value", dest="b", help="B integers", type=int)
     args = parser.parse_args()
-    
+
     # configparser
     print(config.sections())
     config.read(folder + "example.cfg")
@@ -17,7 +17,7 @@ def main():
     for key in config["SectionOne"]:
         print(key)
     print(config["SectionOne"]["status"])
-    
+
     # argparse
     print(args)
     print(args.a)
