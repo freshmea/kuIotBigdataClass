cc77f61897700b545525093ba41a9ab40ea7412b
freshmea <freshmea@naver.com>
2024-05-27 14:19:37 +0900

    sp500

--- pythonData/corRelation.py
+++ pythonData/corRelation.py
@@ -7,9 +7,13 @@ from statsmodels import robust
 
 def main():
     folder = "/home/aa/kuIotBigdataClass/pythonData/data/"
-    sp500_data = pd.read_csv(folder + "sp500_data.csv")
-    sp500_px = pd.read_csv(folder + "sp500_sectors.csv")
-    
+    sp500_px = pd.read_csv(folder + "sp500_data.csv", index_col=0)
+    sp500_sym = pd.read_csv(folder + "sp500_sectors.csv")
+
+    etfs = sp500_px.loc[sp500_px.index > '2012-07-01', sp500_sym[sp500_sym['sector'] == 'etf']['symbol']]
+    sns.heatmap(etfs.corr(), vmin=-1, vmax=1, cmap=sns.diverging_palette(20, 220, as_cmap=True))
+    plt.show()
+
 
 
 if __name__ == "__main__":
