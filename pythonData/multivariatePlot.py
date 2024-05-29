dc03324a5b9d8a79e051c4ba09f76b6cec24edfc
freshmea <freshmea@naver.com>
2024-05-29 11:47:30 +0900

    airline violint plot

--- pythonData/multivariatePlot.py
+++ pythonData/multivariatePlot.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+import seaborn as sns
 
 
 def main():
@@ -13,11 +15,19 @@ def main():
     print(kc_tax0.head())
     print(kc_tax0.info())
     
-    ax = kc_tax0.plot.hexbin(x='SqFtTotLiving', y='TaxAssessedValue', gridsize=30, sharex=False, figsize=(5, 4))
-    ax.set_xlabel('Finished Square Feet')
-    ax.set_ylabel('Tax Assessed Value')
-    plt.tight_layout()
-    plt.show()
+    # ax = kc_tax0.plot.hexbin(x='SqFtTotLiving', y='TaxAssessedValue', gridsize=30, sharex=False, figsize=(5, 4))
+    # ax.set_xlabel('Finished Square Feet')
+    # ax.set_ylabel('Tax Assessed Value')
+    # plt.tight_layout()
+    # plt.show()
+    
+    # 등고선 그리기
+    # fig, ax = plt.subplots(figsize=(4, 4))
+    # kc_tax0.replace([np.inf, -np.inf], np.nan, inplace=True)
+    # ax = sns.kdeplot(data=kc_tax0, x='SqFtTotLiving', y='TaxAssessedValue', ax=ax)
+    # ax.set_xlabel('Finished Square Feet')
+    # ax.set_ylabel('Tax Assessed Value')
+    # plt.show()
 
 
 if __name__ == "__main__":
