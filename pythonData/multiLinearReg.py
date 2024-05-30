47bac99170490a3b2dc14613554f248e0671f02d
freshmea <freshmea@naver.com>
2024-05-30 17:21:59 +0900

    aa

--- pythonData/multiLinearReg.py
+++ pythonData/multiLinearReg.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import statsmodels.api as sm
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
 
 
 def main():
@@ -26,7 +28,20 @@ def main():
     test_data = pd.Series([2500, 5000, 3, 4, 7], index=predictors)
     print(house_lm.predict([test_data])) #type: ignore
     
-    
+    #RMSE
+    print(f"RMSE: {np.sqrt(np.mean(regidue**2)):.3f}")
+    print(f"RMSE: {np.sqrt(mean_squared_error(house[outcome],house_lm.predict(house[predictors]))):.3f}")
+    r2 = r2_score(house[outcome], house_lm.predict(house[predictors]))
+    print(f"R2: {r2:.3f}")
+    model = sm.OLS(house[outcome], house[predictors])
+    results = model.fit()
+    print(results.summary())
+    # RSE
+    print(f"RSE: {np.sqrt(results.mse_resid):.3f}")
+    # MAE (Mean Absolute Error)
+    print(f"MAE: {np.mean(np.abs(regidue)):.3f}")
+    # MAPE (Mean Absolute Percentage Error)
+    print(f"MAPE: {np.mean(np.abs(regidue/house[outcome])):.3f}")
 
 if __name__ == "__main__":
     main()
