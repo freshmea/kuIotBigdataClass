8545f5e437a1c05158a1add88e4e2a2722b22f71
freshmea <freshmea@naver.com>
2024-06-10 16:21:06 +0900

    aa

--- pythonML/testAndSplit.py
+++ pythonML/testAndSplit.py
@@ -5,13 +5,13 @@ from sklearn.model_selection import train_test_split
 def main():
     X = np.arange(10).reshape((5, 2))
     y = range(5)
-    print(f"X : {X}")
-    print(f"y : {y}")
+    print(f"X : \n{X}")
+    print(f"y : \n{y}")
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-    print(f"X_train : {X_train}")
-    print(f"X_test : {X_test}")
-    print(f"y_train : {y_train}")
-    print(f"y_test : {y_test}")
+    print(f"X_train : \n{X_train}")
+    print(f"X_test : \n{X_test}")
+    print(f"y_train : \n{y_train}")
+    print(f"y_test : \n{y_test}")
 
 if __name__ == '__main__':
     main()
