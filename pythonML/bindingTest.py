0ff41aacd5da7c63ed1f4e1959d8dfec9edbe655
freshmea <freshmea@naver.com>
2024-06-10 14:32:27 +0900

    aa

--- pythonML/bindingTest.py
+++ pythonML/bindingTest.py
@@ -9,7 +9,13 @@ def main():
         'preTestScore': [4, 24, 31, 2, 3, 4, 24, 31, 2, 3, 2, 3],
         'postTestScore': [25, 94, 57, 62, 70, 25, 94, 57, 62, 70, 62, 70]
     }
-    
+    df = pd.DataFrame(raw_data)
+    print(df)
+
+    bins = [ i for i in range(0, 101, 25)]
+    group_names = ['Low', 'Okay', 'Good', 'Great']
+    cat = pd.cut(df['postTestScore'], bins, labels=group_names)
+    print(cat)
 
 if __name__ == '__main__':
     main()
