320334bc10ebbf8fdd3c635d8ef6c6edd3d0a9dc
freshmea <freshmea@naver.com>
2024-06-10 12:23:13 +0900

    drop

--- pythonML/dropProcessing.py
+++ pythonML/dropProcessing.py
@@ -11,12 +11,34 @@ def main():
         'preTestScore' : [4, np.nan, np.nan, 2, 3],
         'postTestScore' : [25, np.nan, np.nan, 62, 70]
     }
-    
+
     # df = pd.DataFrame(raw_data, columns = ['first_name', 'last_name', 'age', 'sex', 'preTestScore', 'postTestScore'])
     df = pd.DataFrame(raw_data)
     print(df)
     print(df.isnull().sum()/len(df))
     print(df.info())
+
+    # drop
+    df_nan_drop = df.dropna()
+    print(df.dropna())
+    print(df)
+    print(df_nan_drop)
+
+    # drop all
+    df_nan_drop_all = df.dropna(how='all')
+    print('drop all')
+    print(df_nan_drop_all)
+    df_nan_drop_row = df_nan_drop_all.dropna(axis=1)
+    print(df_nan_drop_row)
+
+    # drop threshold
+    print('drop threshold')
+    print(df.dropna(thresh=3))
+    print(df.dropna(axis=0, thresh=5))
+
+    # drop inplace
+    print(df.dropna(inplace=True))
+
 
 if __name__ == '__main__':
     main()
