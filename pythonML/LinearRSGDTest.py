fac64eb3b5fe614743a8efba1d66cb1d8a9e220a
freshmea <freshmea@naver.com>
2024-06-11 14:07:35 +0900

    aa

--- pythonML/LinearRSGDTest.py
+++ pythonML/LinearRSGDTest.py
@@ -47,13 +47,13 @@ class LinearRegressionGD(object):
                 np.random.shuffle(X_copy)
 
             batch = len(X_copy)//self.batch_size
-            
+
             for batch_count in range(batch):
                 X_batch = np.copy(X_copy[batch_count*self.batch_size:(batch_count+1)*self.batch_size])
                 y_batch = np.copy(y[batch_count*self.batch_size:(batch_count+1)*self.batch_size])
                 gradient = self.gradient(X_batch, y_batch, theta).flatten()
                 theta = theta - self._eta0 * gradient
-                
+
             if epoch % 100 == 0:
                 self._w_history.append(theta)
                 cost = self.cost(
@@ -90,14 +90,7 @@ class LinearRegressionGD(object):
         return self._cost_history
 
 def main():
-    # folder = "/home/aa/kuIotBigdataClass/pythonData/data/"
-    # df = pd.read_csv(folder +"house_sales.csv", sep="\t")
-    # predictors = "SqFtTotLiving"
-    # target = "SalePrice"
-    # X = df[predictors].values.reshape(-1, 1)
-    # y = df[target].values
-    # print(X.shape, y.shape)
-    
+
     folder = "/home/aa/kuIotBigdataClass/pythonML/"
     df = pd.read_csv(folder + "boston.csv")
     print(df.info())
@@ -111,6 +104,7 @@ def main():
     x_scaled_data = minmax_scale.transform(x_data)
     X = x_scaled_data
     y = y_data
+    
     gd_lr = LinearRegressionGD(eta0=0.001, epochs=1000, shuffle=False)
     bgd_lr = LinearRegressionGD(eta0=0.001, epochs=1000, shuffle=False, batch_size=X.shape[0])
     sgd_lr = LinearRegressionGD(eta0=0.001, epochs=1000, shuffle=True )
@@ -124,17 +118,17 @@ def main():
     sgd_lr.fit(X, y)
     print("Mini-Batch Stochastic Gradient Descent")
     msgd_lr.fit(X, y)
-    
+
     import matplotlib.pyplot as plt
     plt.plot(range(len(gd_lr.cost_history)), gd_lr.cost_history, label="GD", c="r")
     plt.plot(range(len(bgd_lr.cost_history)), bgd_lr.cost_history, label="BGD", c="g")
     plt.plot(range(len(sgd_lr.cost_history)), sgd_lr.cost_history, label="SGD", c="b")
     plt.plot(range(len(msgd_lr.cost_history)), msgd_lr.cost_history, label="MSGD", c="y")
-    
+
     plt.show()
-    print(f"w history gd:{gd_lr.weights_history}")
-    print(f"w history bgd:{bgd_lr.weights_history}")
-    print(f"w history sgd:{sgd_lr.weights_history}")
-    print(f"w history msgd:{msgd_lr.weights_history}")
+    # print(f"w history gd:{gd_lr.weights_history}")
+    # print(f"w history bgd:{bgd_lr.weights_history}")
+    # print(f"w history sgd:{sgd_lr.weights_history}")
+    # print(f"w history msgd:{msgd_lr.weights_history}")
 if __name__ == "__main__":
     main()
