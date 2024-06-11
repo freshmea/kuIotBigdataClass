b562951ca634df3a9dc2296102749a61da5f4799
freshmea <freshmea@naver.com>
2024-06-11 11:30:24 +0900

    aa

--- pythonML/bostonHouseRegression.py
+++ pythonML/bostonHouseRegression.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 
@@ -5,6 +7,12 @@ def main():
     folder = "/home/aa/kuIotBigdataClass/pythonML/"
     df = pd.read_csv(folder + "boston.csv")
     print(df.info())
+    print(df.keys())
+    data = df.drop(columns=["MEDV"])
+    target = df["MEDV"]
+    x_data = data.to_numpy()
+    y_data = target.to_numpy().reshape(-1, 1)
+    print(y_data.shape)
 
 if __name__ == '__main__':
     main()
