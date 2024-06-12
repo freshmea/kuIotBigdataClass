8c283606bbde070770fa8f72114aa23b759018d6
freshmea <freshmea@naver.com>
2024-06-12 15:56:44 +0900

    aa

--- pythonML/baggingProcess.py
+++ pythonML/baggingProcess.py
@@ -3,7 +3,7 @@ import random
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import BaggingClassifier, RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import (
@@ -38,6 +38,7 @@ def main():
     X_train = X_oneHot.iloc[:891, :]
     y_train = y.iloc[:891]
 
+    # bagging ensemble(LogisticRegression)
     clf1 = LogisticRegression(random_state=1)
     eclf = BaggingClassifier(clf1, oob_score=True)
     print(cross_val_score(eclf, X_train, y_train, cv=5).mean())
@@ -47,6 +48,20 @@ def main():
     }
     grid = GridSearchCV(estimator=eclf, param_grid=params, cv=5)
     grid.fit(X_train, y_train)
+    print(grid.best_params_)
+    print(grid.best_score_)
+
+    # randomforst ensemble
+    eclf = RandomForestClassifier(n_estimators=100, max_features=2, n_jobs=7, oob_score=True)
+    eclf.fit(X_train, y_train)
+    print(cross_val_score(eclf, X_train, y_train, cv=5).mean())
+    params = {
+        "n_estimators": [10, 20, 30, 50, 100],
+        "max_samples": [1,2,3,4,5,6,7,10,15,20,25, len(X_train[0])],
+    }
+    grid = GridSearchCV(estimator=eclf, param_grid=params, cv=5)
+    grid.fit(X_train, y_train)
+
     print(grid.best_params_)
     print(grid.best_score_)
 
